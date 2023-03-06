b0e62f55929903c01dba079ee575ed41bcbc229d
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-06 11:26:33 +0200

    4. Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,11 +14,25 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Checks if a path requires authentication.
-        :param path:
-        :param excluded_paths:
+        :param path: path to be checked.
+        :param excluded_paths: list of paths excluded.
         :return:
+            - True if path is None
+            - True if excluded_paths is None or empty.
+            - False if path is in excluded_paths
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
