a73cbfd7f6f1e80ba81f5339d3e45c51abd7cd5f
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-07 11:52:12 +0200

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,9 +15,7 @@ class BasicAuth(Auth):
     """Basic authentication class.
     """
 
-    def extract_base64_authorization_header(
-            self,
-            authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
         Extracts the Base64 part of the Authorization header
         for a Basic Authentication.
@@ -28,3 +26,16 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str,) -> str:
+        """Decodes a base64-encoded authorization header.
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
