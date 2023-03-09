32c11e8d84380bda0c020c4b0c124b95c756fd1b
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-09 10:23:29 +0000

    Complete assignment

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -53,3 +53,11 @@ class Auth:
         :return: None
         """
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """
+        Gets the value of the cookie named SESSION_NAME.
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
