a05d9727f312c2031fc6d9602c780159fedd8b07
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-13 11:03:05 +0000

    correct indentation.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+Route module for the API.
 """
 import os
 from os import getenv
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
@@ -19,16 +19,16 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
+auth_type = getenv("AUTH_TYPE", "auth")
+if auth_type == "auth":
     auth = Auth()
-if auth_type == 'basic_auth':
+if auth_type == "basic_auth":
     auth = BasicAuth()
-if auth_type == 'session_auth':
+if auth_type == "session_auth":
     auth = SessionAuth()
-if auth_type == 'session_exp_auth':
+if auth_type == "session_exp_auth":
     auth = SessionExpAuth()
-if auth_type == 'session_db_auth':
+if auth_type == "session_db_auth":
     auth = SessionDBAuth()
 
 
@@ -70,8 +70,10 @@ def authenticate_user():
         ]
         if auth.require_auth(request.path, excluded_paths):
             user = auth.current_user(request)
-            if auth.authorization_header(request) is None and \
-                    auth.session_cookie(request) is None:
+            if (
+                auth.authorization_header(request) is None
+                and auth.session_cookie(request) is None
+            ):
                 abort(401)
             if user is None:
                 abort(403)
@@ -82,3 +84,4 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+
